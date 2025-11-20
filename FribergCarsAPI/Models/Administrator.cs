4f110a87d456479d3e57acfb3c6de65c17e91138
RS9K <rey97smith@gmail.com>
2025-11-20 13:31:43 +0100

    1. VS Community update 2022 --> 2026 2. File error:   - Moved /FribergCarsAPI/ out of FribergCars.csproj

--- FribergCarsAPI/Models/Administrator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FribergCars.Models
-{
-    public class Administrator
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
